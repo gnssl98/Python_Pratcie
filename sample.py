da7ec2f90877f520f58383f7163b4eaa3c25c958
gnssl98 <gnssl98@naver.com>
2018-07-24 19:21:41 +0900

    sample.py

--- sample.py
+++ sample.py
@@ -1,23 +1,48 @@
 class Seller(object):
-    def __init__(self, apple_money, pocket):
-        self.apple_money = apple_money
-        self.pocket =pocket
+    def __init__(self, price_apple, num_apple):
+        self.price_apple = price_apple
+        self.num_apple = num_apple
 
-    def cal(self):
-        return self.pocket
+    def cal(self, money):
+        apple_num = money // self.price_apple
+        pay = apple_num * self.price_apple
+        self.num(apple_num)
+        if apple_num < 1 :
+            print('Not enough Money')
+        else:
+            self.change(money, apple_num, pay)
+            self.pay(pay)
 
+    def num(self, apple_num):
+        self.num_apple -= apple_num
+
+    def change(self, money, apple_num, total_money):
+        change = money - (total_money)
+        print('Change : {}'.format(change))
+
+    def pay(self, pay):
+        print('Pay: {}'.format(pay))
+
+
+    def main(self, money):
+        print('---------------------\nSeller\n---------------------')
+        self.cal(money)
+        print('Num of Apple : {}'.format(self.num_apple))
 
 class Buyer(object):
-    def __init__(self, pocket, count, who, pay):
-        self.pocket = pocket
-        self.count  = count
-        self.who = who
-        self.pay = pay
+    def __init__(self, money):
+        self.money = money
 
-    def pay(self):
-        self.pay = self.count * self.who.apple_money
-        self.who.cal = self.pay
+    def order(self, seller, money):
+       print('---------------------\nBuyer\n---------------------')
+       print('Money : {}\nMoney to buy apple : {}\nTo Buy : {}\n'.format(self.money,money, seller))
+       seller.main(money)
 
 
-seller1 = Seller(3000)
-buyer1 = Buyer(300000, 10, seller1)
+seller1 = Seller(3000, 50)
+buyer1 = Buyer(300000)
+buyer1.order(seller1, 10000)
+
+seller2 = Seller(5000, 100)
+buyer2 = Buyer(4000000)
+buyer2.order(seller2, 10000)
