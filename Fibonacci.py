45d4b1997585cf677316d1b312dbd817fd7a2f90
gnssl98 <gnssl98@naver.com>
2018-07-30 20:48:57 +0900

    Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,15 +1,16 @@
 class Fibo(object):
     def __init__(self):
-        self.Fibo_number = list()
-        self.Fibo_number[0:1] = 1, 1
+        self.Fibo_number = [1, 1]
 
     def cal(self, Input_number):
         for Input_number in range(2, Input_number):
             temp = self.Fibo_number[Input_number - 1] + self.Fibo_number[Input_number - 2]
             self.Fibo_number.append(temp)
+        return self.Fibo_number
 
     def __str__(self):
         return "{}".format(self.Fibo_number[self.cal.Input_number])
 
-fibo1 = Fibo.cal(6)
-print(fibo1)
+fibo1 = Fibo()
+result = fibo1.cal(6)
+print(result)
