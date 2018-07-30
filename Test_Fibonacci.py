4d1828a79ce1d84f456f351ebe8dd1d0a3035fd4
gnssl98 <gnssl98@naver.com>
2018-07-30 20:49:18 +0900

    Test_Fibonacci.py

---
+++ Test_Fibonacci.py
@@ -0,0 +1,10 @@
+from Fibonacci import Fibo
+
+class test_Fibo(Fibo):
+    def cal(self, Input_number):
+        for Input_number in range(2, Input_number):
+            temp = self.Fibo_number[Input_number -1 ] + self.Fibo_number[Input_number - 2]
+            temp != 0, "List is Empty"
+
+Fibo = []
+
