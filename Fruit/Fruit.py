5ec61aed5ac9c8ecb9e58751d811f9af0358cf45
gnssl98 <gnssl98@naver.com>
2018-08-08 00:15:24 +0900

    Fruit/

--- Fruit/Fruit.py
+++ Fruit/Fruit.py
@@ -25,6 +25,8 @@ class Seller(object):
             raise  ValueError("Not enough Money")
         else :
             self.num_apples -= num_apples
+            if self.num_apples < 0:
+                raise ValueError("Not enough Apples")
             money = num_apples * self.price_apple
             self.money += money
             change = order_money - money
@@ -33,14 +35,17 @@ class Seller(object):
     def __str__(self):
         return "-------------------------\nSeller\n-------------------------\nNum of Apples : {}\nMoney : {}".format(self.num_apples, self.money)
 
+def main():
+    seller1 = Seller(3000, 60)
+    buyer1 = Buyer(100000)
+    buyer1.order(seller1, 10000)
+    print(buyer1)
+    buyer1.order(seller1, 30000)
+    print(buyer1)
+    print(seller1)
+    buyer1.order(seller1, 2000)
+    print(seller1)
+    print(buyer1)
 
-seller1 = Seller(3000, 60)
-buyer1 = Buyer(100000)
-buyer1.order(seller1, 10000)
-print(buyer1)
-buyer1.order(seller1, 30000)
-print(buyer1)
-print(seller1)
-buyer1.order(seller1, 2000)
-print(seller1)
-print(buyer1)
+if __name__ == "__main__":
+    main()
