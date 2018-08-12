690c205139e708355f3022842aa501142bd84f46
gnssl98 <gnssl98@naver.com>
2018-08-12 21:24:39 +0900

    Fruit_Broker.py

--- Fruit_Broker/Fruit_Broker.py
+++ Fruit_Broker/Fruit_Broker.py
@@ -11,24 +11,22 @@ class Seller(object):
             self.num_apples = 0
             sell_num_apples
             return sell_num_apples
-        else : 
+        else:
             return order_num_apples
             self.num_apples -= order_num_apples
             sell_money = order_num_apples * self.price_apple
             self.money += sell_money
             
     def __str__(self):
-        return "--------------------------\bSeller\n-----------------------\nNum of Apples :{}\nMoney :{} ".format(self.num_apples, self.money)
+        return "--------------------------\nSeller\n-----------------------\nNum of Apples :{}\nMoney :{} ".format(self.num_apples, self.money)
             
 
 class Buyer(object):
     def __init__(self, money):
         self.money = money
         self.num_apples = 0
-    
 
-    def order(self, order_money):
-        broker = Broker()
+    def order(self, broker, order_money):
         sell_money, num_apples = broker.cal(order_money)
         self.money -= sell_money 
         self.num_apples += num_apples
@@ -38,13 +36,13 @@ class Buyer(object):
         
 
 class Broker(object):
-    def __init__(self):
-        self.cheap_seller = sellers[0]
-        self.sellers = []
-        self.money = 0
-    
-    def select_cheap_seller(self, sellers):
+    def __init__(self, sellers):  
         self.sellers = sellers
+        self.cheap_seller = sellers[0] 
+        self.money = 0
+        self.commision = 0
+   
+    def select_cheap_seller(self):
         for i in self.sellers:
             if self.cheap_seller.price_apple > self.sellers[i].price_apple:
                 self.cheap_seller = self.sellers[i]
@@ -52,39 +50,37 @@ class Broker(object):
     
     def cal(self, order_money):
         sell_money = order_money // self.cheap_seller.price_apple
-        commission = sell_money * 1.1
-        if order_money < sell_money + commission:
+        self.commission = sell_money * 1.1
+        if order_money < sell_money + self.commission:
             raise ValueError("Not enough Money")
-        else :
-            return sell_money , self.sell_fruit(order_money)
+        else:
+            return sell_money, self.sell_fruit(order_money)
     
     def sell_fruit(self, order_money):
         cheap_seller = self.cheap_seller
         num_apples = cheap_seller.cal(order_money)
         order_num_apples = order_money // cheap_seller.price_apple
-        if num_apples < order_num_apples :
-            self.sellers.remove(self.cheap_seller)
-            self.cheap_seller = select_cheap_seller(self.sellers)
+        if num_apples < order_num_apples:
+            self.sellers.remove(cheap_seller)
+            self.cheap_seller = select_cheap_seller(self)
             re_order_money = (order_num_apples - num_apples) * self.cheap_seller.price_apple
             self.cal(re_order_money)
         
-        self.money += self.cal.commission
+        self.money += self.commission
         return num_apples  
     
     def __str__(self):
-        return "-------------------------\nBroker\n----------------------\nNum of Apples: {} \nMoney: {}".foramt(self.sell_fruit.num_apples, self.money)
+        return "-------------------------\nBroker\n----------------------\nCommision: {} \nMoney: {}".format(self.commission, self.money)
 
 
 def main():
-    sellers = [ Seller(3000,60), Seller(5000,50), Seller(2000, 30), Seller(1000, 10)]
-    broker = Broker()
-    broker.select_cheap_seller(sellers)
+    sellers = [ Seller(3000, 60), Seller(5000, 50), Seller(2000, 30), Seller(1000, 10)]
+    broker = Broker(sellers)
     buyer1 = Buyer(100000)
-    buyer1.order(10000)
+    buyer1.order(broker, 10000)
     print(broker.cheap_seller)
     print(broker)
-    print(buyer)
+    print(buyer1)
 
-    
 if __name__ == "__main__" :
     main()
