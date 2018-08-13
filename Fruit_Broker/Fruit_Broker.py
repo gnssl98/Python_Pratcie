0832d6f4b126645623678af2568151a441a0c727
gnssl98 <gnssl98@naver.com>
2018-08-13 15:28:39 +0900

    Fruit_Broker.py

--- Fruit_Broker/Fruit_Broker.py
+++ Fruit_Broker/Fruit_Broker.py
@@ -4,18 +4,18 @@ class Seller(object):
         self.num_apples = num_apples
         self.money = 0
 
-    def cal(self, order_money):
-        order_num_apples = order_money // self.price_apple
+    def cal(self, sell_money):
+        order_num_apples = sell_money // self.price_apple
         if order_num_apples >= self.num_apples:
             sell_num_apples = self.num_apples
             self.num_apples = 0
-            sell_num_apples
+            self.money += self.num_apples * self.price_apple 
             return sell_num_apples
         else:
-            return order_num_apples
             self.num_apples -= order_num_apples
             sell_money = order_num_apples * self.price_apple
             self.money += sell_money
+            return order_num_apples
             
     def __str__(self):
         return "--------------------------\nSeller\n-----------------------\nNum of Apples :{}\nMoney :{} ".format(self.num_apples, self.money)
@@ -41,28 +41,37 @@ class Broker(object):
         self.cheap_seller = sellers[0] 
         self.money = 0
         self.commision = 0
+        self.change = 0
    
     def select_cheap_seller(self):
-        for i in self.sellers:
-            if self.cheap_seller.price_apple > self.sellers[i].price_apple:
-                self.cheap_seller = self.sellers[i]
+        for seller in self.sellers:
+            if self.cheap_seller.price_apple > seller.price_apple:
+                self.cheap_seller = seller
         return self.cheap_seller
     
     def cal(self, order_money):
-        sell_money = order_money // self.cheap_seller.price_apple
-        self.commission = sell_money * 1.1
-        if order_money < sell_money + self.commission:
+        self.select_cheap_seller()
+        num_apples = order_money // self.cheap_seller.price_apple
+        sell_money = num_apples * self.cheap_seller.price_apple
+        self.commission = sell_money * 0.1
+        if num_apples == 0:
             raise ValueError("Not enough Money")
+        elif order_money <= sell_money + self.commission:
+            num_apples -= 1
+            sell_money = num_apples * self.cheap_seller.price_apple
+            self.commission = sell_money * 0.1
+            return sell_money + self.commission, self.sell_fruit(sell_money, num_apples)
         else:
-            return sell_money, self.sell_fruit(order_money)
+            return sell_money + self.commission, self.sell_fruit(sell_money, num_apples)
+        self.change = order_money - sell_money - self.commission    
     
-    def sell_fruit(self, order_money):
+    def sell_fruit(self, sell_money, order_num_apples):
         cheap_seller = self.cheap_seller
-        num_apples = cheap_seller.cal(order_money)
-        order_num_apples = order_money // cheap_seller.price_apple
+        num_apples = cheap_seller.cal(sell_money)
         if num_apples < order_num_apples:
-            self.sellers.remove(cheap_seller)
-            self.cheap_seller = select_cheap_seller(self)
+            if cheap_seller in self.sellers:
+                self.sellers.remove(cheap_seller)
+            self.cheap_seller = self.select_cheap_seller()
             re_order_money = (order_num_apples - num_apples) * self.cheap_seller.price_apple
             self.cal(re_order_money)
         
@@ -70,7 +79,7 @@ class Broker(object):
         return num_apples  
     
     def __str__(self):
-        return "-------------------------\nBroker\n----------------------\nCommision: {} \nMoney: {}".format(self.commission, self.money)
+        return "-------------------------\nBroker\n----------------------\nCommision: {} \nMoney: {}\nChange : {}".format(self.commission, self.money, self.change)
 
 
 def main():
@@ -81,6 +90,12 @@ def main():
     print(broker.cheap_seller)
     print(broker)
     print(buyer1)
+    buyer2 = Buyer(100000)
+    buyer2.order(broker, 40000)
+    print(broker.cheap_seller)
+    print(broker)
+    print(buyer2)
+
 
 if __name__ == "__main__" :
     main()
