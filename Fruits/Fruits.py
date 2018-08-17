a7becea3d489c09662b83e01deed5ab67b4598ce
gnssl98 <gnssl98@naver.com>
2018-08-17 12:01:18 +0900

    Fruits.py

--- Fruits/Fruits.py
+++ Fruits/Fruits.py
@@ -1,36 +1,34 @@
 class Fruit_Seller(object):
-    def __init__(self, list_fruits):
-        self.Fruits = list_fruits
+    def __init__(self, dict_fruits):
+        self.Fruits = dict_fruits
         self.money = 0
 
     def sell_fruit(self, fruit, order_money):
-        num_fruits = self.Fruits.get('fruit')
-        
-        
+        info_fruits = self.Fruits.get('fruit')
+        order_nums_fruits = order_money // info_fruit[1]
+        sell_money = order_nums_fruits * info_fruit[1]
+        self.money += sell_money
+        return sell_money, order_num_fruits
+       
+    def __str__(self):
+        return "----------------------\nSeller\n-------------------Num of {} : {} ".format(fruit, num_fruits)
         
 
 
 class Fruit_Buyer(object):
     def __init__(self, money):
-        sefl.money = money
+        self.money = money
+        self.info_fruits = []
     
     def order(self, fruit, order_money):
-        Fruit_Seller.sell_fruit(fruit, order_money)
+        sell_money , order_num_fruits = Fruit_Seller.sell_fruit(fruit, order_money)
+        self.money += sell_money
+        self.info_fruits = [{},{}].format(fruit, sell_money)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-list_fruits = { 'apple' : [30, 1000], 'orange' : [20, 2000], 'grape' : [40, 2000]}
-seller1 = seller(list_fruits)
-buyer = Buyer(100000)
+dict_fruits = { 'apple' : [30, 1000], 'orange' : [20, 2000], 'grape' : [40, 2000]}
+seller1 = Fruit_Seller(dict_fruits)
+buyer = Fruit_Buyer(100000)
 buyer.order('orange', 3000)
